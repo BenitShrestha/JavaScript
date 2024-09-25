27f2cdf1740b274d2a05e9dfbc3d346ce2323e2f
BenitShrestha <quinjet01234@gmail.com>
2024-09-25 10:49:40 +0545

    Module for while loops
    Includes do while loop

---
+++ ChapterThree/WhileLoops.js
@@ -0,0 +1,21 @@
+console.log("While Loops")
+
+/* let num = Number.parseInt(prompt("Enter a number"))
+let i = 0
+
+while(i < num) {
+    console.log(num)
+    console.log(i)
+    num--
+    i++
+}
+
+console.log("Outside: " + i) */
+
+// Do while
+
+num = 10
+do { // Do while will run at least once
+    console.log(num)
+    num--
+}while(num > 2)
