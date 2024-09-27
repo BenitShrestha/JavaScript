870e4d51a9cf6809f3507a93f298212edef72987
BenitShrestha <quinjet01234@gmail.com>
2024-09-27 17:10:46 +0545

    Element and Table Nav

--- ChapterSeven/WalkingTheDOM.js
+++ ChapterSeven/WalkingTheDOM.js
@@ -3,7 +3,7 @@ console.log("Walking the DOM")
 // DOM Tree --> HTML --> Head, Body --> Elements
 // Text nodes, Element nodes, Comment nodes
 
-console.log(document.body) // Detailed view in the console
+/* console.log(document.body) // Detailed view in the console
 console.log(document.head)
 console.log(document.documentElement)
 
@@ -25,4 +25,66 @@ console.log(document.documentElement.parentNode)
 console.log(document.body.firstChild.nextSibling) // Next sibling of first child of body
 console.log(document.body.firstElementChild.nextElementSibling) // Next element sibling of first child element of body
 
-console.log(document.body.childNodes[1].previousSibling) // Previous sibling
+console.log(document.body.childNodes[1].previousSibling) // Previous sibling */
+
+// Element only navigation
+/* console.log("\nElement only Navigation")
+
+// Nodes - Text, Comment, Elements, Spaces too
+// Elements - Tags
+
+console.log(document.body.childNodes) // Space counted as Text node
+console.log(document.body.childElementCount)
+
+let secondChild_body = document.body.childNodes[5]
+console.log(secondChild_body)
+console.log(secondChild_body.childNodes) // Child of second child element (nav)
+
+document.body.firstElementChild.style.background = 'pink'
+document.body.childNodes[5].childNodes[1].childNodes[1].style.background = 'pink' // Makes Home pink, check HTML file\
+// CSS manipulation from JS
+
+let li_aboutme = document.body.childNodes[5].childNodes[1].childNodes[3]
+console.log(li_aboutme)
+li_aboutme.style.background = 'yellow'
+
+// A lot of hierarchy needs to be understood for element only navigation */
+
+// Table Navigation -- Linked to table.html
+console.log('\nTable Navigation')
+
+// let table = document.querySelector('table') // Same as below
+let table = document.body.firstElementChild.firstElementChild
+console.log(table)
+
+console.log('Rows: ')
+console.log(table.rows) 
+
+console.log("Columns: ")
+console.log(table.columns) // Doesn't work
+
+console.log(table.tHead) // Top row, header
+console.log(table.tFoot) // Bottom row, footer
+
+console.log(table.tBodies) // Table body
+
+let tablerow = document.body.firstElementChild.firstElementChild.rows[1] // Second row, below header
+console.log(tablerow)
+
+console.log(tablerow.cells) // Individual Cells
+
+console.log('Section Row Index')
+console.log(tablerow.sectionRowIndex) // Index of row, excludes the header: 0
+console.log(document.getElementById('row3').sectionRowIndex) // Lies below the row above: 1
+console.log(document.getElementById('table-header').sectionRowIndex) // Is zero as well
+
+console.log('Row Index')
+console.log(tablerow.rowIndex) // Overall row index
+
+console.log('Cell Index')
+cells_in_row = tablerow.cells
+console.log(cells_in_row) // Individual cells
+console.log(cells_in_row[0]) // First cell
+console.log(cells_in_row[2].cellIndex) // Gives index of cell at that position
+
+console.log('Type of Document: ' + typeof document + "\nType of Window: " + typeof window) // Both objects
