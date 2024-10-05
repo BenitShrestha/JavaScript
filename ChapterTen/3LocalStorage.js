fb4460d5d9a3bc52b845bf4951bc362d21cf4f19
BenitShrestha <quinjet01234@gmail.com>
2024-10-05 18:41:52 +0545

    Session Storage & Storage Events

--- ChapterTen/3LocalStorage.js
+++ ChapterTen/3LocalStorage.js
@@ -6,11 +6,11 @@ console.log("Local Storage" )
 
 localStorage.setItem('key1', 'value1')
 
-let key = prompt("Enter a key: ")
+/* let key = prompt("Enter a key: ")
 let value = prompt("Enter a value: ")
 
 localStorage.setItem(key, value)
-console.log(localStorage.getItem(key)) // Returns value
+console.log(localStorage.getItem(key)) // Returns value */
 
 // localStorage.removeItem('key1') // (key), removes the key-value pair
 
@@ -31,3 +31,16 @@ alert(localStorage.one)
 delete localStorage.one
 
 // Session Storage
+console.log("\n\nSession Storage")
+
+// Like local storage but only persists for the current session, shares the same methods.
+// Unique to one tab, doesn't get removed when tab is refreshed but is removed if tab is closed.
+
+sessionStorage.setItem('key1', 'value1') // For unique session key-value pairs, used console terminal of webpage
+
+// Storage events - Function can be triggered when changes are made to storage
+
+window.onstorage = (e) => { // Applicable to local storage only, can be checked for the console terminal
+    alert('Storage Changed')
+    console.log(e) // Event object, provides event details
+}
